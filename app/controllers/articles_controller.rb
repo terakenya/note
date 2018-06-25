3f6fc865c6cb93503827710602e4f8664008b3a0
terakenya <acmacm10@icloud.com>
2018-06-25 15:48:19 +0900

    post_article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,12 +10,15 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def create
-    Article.create(text: article_params[:text])
+    # Article.create(text: article_params[:text])
+    @article = Article.new(article_params)
+    @article.save
     redirect_to articles_path
   end
 
   private
   def article_params
-    params.permit(:text)
+    params.require(:article).permit(:image, :content, :title)
+
   end
 end

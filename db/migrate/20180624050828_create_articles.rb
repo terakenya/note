d04042880b105f88dcc0f977f81aa0a28c6ff626
terakenya <acmacm10@icloud.com>
2018-06-26 10:28:55 +0900

    post_article

--- db/migrate/20180624050828_create_articles.rb
+++ db/migrate/20180624050828_create_articles.rb
@@ -1,6 +1,7 @@
 class CreateArticles < ActiveRecord::Migration[5.0]
   def change
     create_table :articles do |t|
+      t.references :user, foreign_key: true
       t.string :title
       t.text :content
       t.text :image

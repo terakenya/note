0dc45caac75436572d63632c8e8b189addbea4c0
terakenya <acmacm10@icloud.com>
2018-06-26 17:34:30 +0900

    delete_article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,26 @@
 class ArticlesController < ApplicationController
 
   def index
+
   end
 
   def show
+    @article = Article.find(params[:id])
+    binding.pry
+  end
 
+  def destroy
+    article = Article.find(params[:id])
+    if article.user_id == current_user.id
+      article.destroy
+    end
+    redirect_to article
   end
 
   def new
     @article = Article.new
   end
+
   def create
     @article = Article.new(article_params)
     @article.save

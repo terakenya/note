e86bdc65eb9c7fac09203a5ec28087dab38ae677
Tsukasa ARITOMI <tsukasaaritomi@aritomi-no-MacBook-Pro-2.local>
2018-06-26 18:23:01 +0900

    edit-function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,6 +26,16 @@ class ArticlesController < ApplicationController
     redirect_to (@article)
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+   def update
+    article = Article.find(params[:id])
+    article.update(article_params) if article.user_id == current_user.id
+    redirect_to action: 'show'
+  end
+
   private
   def article_params
     params.require(:article).permit(:image, :text, :title).merge(user_id: current_user.id)

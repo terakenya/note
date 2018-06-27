92ca2dcc2af926b53e95097e0160d2e4a38809a5
terakenya <acmacm10@icloud.com>
2018-06-27 19:39:16 +0900

    add_comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comments = Comment.all
   end
 
   def destroy

6a8a9237d7fdb7fb60695cbcdf96aa7e8544a157
terakenya <acmacm10@icloud.com>
2018-06-28 19:26:38 +0900

    image_resiza

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fit: [620, 325]
+  process resize_to_fill:[578, 303]
 
   # Choose what kind of storage to use for this uploader:
   storage :file

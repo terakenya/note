91c319cd9b88ed6d38cef8fa40147e4ff0018f57
terakenya <acmacm10@icloud.com>
2018-07-10 19:20:08 +0900

    Js

--- app/assets/javascripts/inclimental.js
+++ app/assets/javascripts/inclimental.js
@@ -1,12 +1,13 @@
 $(function() {
   var users_list = $("#user-search-result");
   function appendUser(user) {
-    // var html = `<div class="chat-group-user clearfix">
-    //             <p class="chat-group-user__name">${user.nickname}</p>
-    //             <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id = ${user.id} data-user-name=${user.nickname}>追加</a>
-    //             </div>
-    //             `
-    users_list.append(html);
+    var html = `<div class="angucomplete-dropdownA">
+                  <div class="angucomplete-row-angucomplete-selected-row"></div>
+                </div>
+                <div class="angucomplete-dropdownB">
+                  <div class="angucomplete-row-angucomplete-selected-row"></div>
+                </div>`
+    angucomplete-dropdown.append(html);
   }
 
   $("#usersearch").on("keyup", function() {
@@ -20,14 +21,14 @@ $(function() {
     })
     .done(function(users) {
       console.log(users)
-      // $("#user-search-result").empty();
-      // if(users.length !== 0) {
-      //   users.forEach(function(user) {
-      //     appendUser(user);
-      //   });
-      // } else {
-      //   appendNoUser();
-      // }
+      $(".angucomplete-dropdown").empty();
+        if(users.length !== 0) {
+          users.forEach(function(user) {
+            appendUser(user);
+          });
+        } else {
+         // appendNoUser();
+        }
     })
   //   .fail(function() {
   //     alert('error');

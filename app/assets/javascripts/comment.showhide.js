02401784f37b0946edc3b15e052b494fdeadec9f
Tsukasa ARITOMI <tsukasaaritomi@aritomi-no-MacBook-Pro-2.local>
2018-07-15 19:34:00 +0900

    add-somefunctions

---
+++ app/assets/javascripts/comment.showhide.js
@@ -0,0 +1,7 @@
+// $(document).on('turbolinks:load', function() {
+//   $(".p-comments__inputMessage").on("click", function() {
+//     $(".p-comments__inputMessage").css('border-color','#2cb696');
+//   });
+// });
+
+
